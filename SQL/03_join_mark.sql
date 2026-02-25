9951ef6760033202b2fe58379ec91e73df9d8f30
Harrison Nordstrom <harrisonnordstrom2@gmail.com>
2026-02-25 12:17:53 -0500

    Update 03_join_mark.sql

--- SQL/03_join_mark.sql
+++ SQL/03_join_mark.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE VIEW `YOUR_PROJECT.readmissions.mart_hrrp_joined` AS
+CREATE OR REPLACE VIEW `readmissions-488017.readmissions.mart_hrrp_joined` AS
 SELECT
-  h.facility_id_norm,
+  r.facility_id_norm,
   COALESCE(h.facility_name, r.facility_name_hrrp) AS facility_name,
   COALESCE(h.state, r.state) AS state,
 
@@ -25,6 +25,21 @@ SELECT
   r.end_date,
   r.suppressed_flag
 
-FROM `YOUR_PROJECT.readmissions.stg_hrrp` r
-LEFT JOIN `YOUR_PROJECT.readmissions.stg_hospital_info` h
-  USING (facility_id_norm);
+FROM `readmissions-488017.readmissions.stg_hrrp` r
+LEFT JOIN (
+  SELECT
+    facility_id_norm,
+    ANY_VALUE(facility_name) AS facility_name,
+    ANY_VALUE(state) AS state,
+    ANY_VALUE(hospital_type) AS hospital_type,
+    ANY_VALUE(hospital_ownership) AS hospital_ownership,
+    ANY_VALUE(emergency_services) AS emergency_services,
+    ANY_VALUE(overall_rating) AS overall_rating,
+    ANY_VALUE(readm_measures_count) AS readm_measures_count,
+    ANY_VALUE(readm_better) AS readm_better,
+    ANY_VALUE(readm_no_diff) AS readm_no_diff,
+    ANY_VALUE(readm_worse) AS readm_worse
+  FROM `readmissions-488017.readmissions.stg_hospital_info`
+  GROUP BY facility_id_norm
+) h
+USING (facility_id_norm);
